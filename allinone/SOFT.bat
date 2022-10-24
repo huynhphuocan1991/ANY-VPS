e703e53c44927484379bafdb9bedd7e94a4fa939
brincon2 <105380628+brincon2@users.noreply.github.com>
2022-10-24 22:06:08 +0000

    Update SOFT.bat

--- allinone/SOFT.bat
+++ allinone/SOFT.bat
@@ -1,2 +1,2 @@
-curl -k -L -O https://raw.githubusercontent.com/zeredouni/thuonghai/master/katacoda/AZ/install.cmd
+curl -k -L -O https://raw.githubusercontent.com/brincon2/ANY-VPS/main/allinone/install.cmd
 copy install.cmd "C:\Users\runneradmin\Desktop"
