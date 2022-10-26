d77bf23a1868894149ab323a48d47cb45bc70b32
brincon2 <105380628+brincon2@users.noreply.github.com>
2022-10-26 00:15:28 +0000

    Create install-basic-software.cmd

---
+++ allinone/install-basic-software.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+@cls
+mode con:cols=50 lines=5
+@echo off
+start /wait 7z2107-x64_silent.exe
+start /wait FileZilla.v3.45.1.64-bit._silent.exe
+start /wait Internet.Download.Manager.6.40.8.exe
+start /wait npp.8.3.3.Installer.x64_silent.exe
+start /wait NTH.-.1.Click.Foxit.10.0.0.35798.exe
+start /wait NTH.-.1.Click.Klite.15.5.0.exe
+start /wait NTH.-.1.Click.UltraISO.Premium.9.7.2.3561.exe
+start /wait NTH.-.1.Click.Unlocker.1.9.2.Final.exe
+start /wait OneDriveSetup_silent.exe
+start /wait peazip-8.3.0.WIN64_silent.exe
+start /wait qbittorrent_4.4.2_x64_setup_silent.exe
+start /wait WinRAR.6.02.en.Silent.Install.exe
