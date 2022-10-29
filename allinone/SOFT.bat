9af81523df15ee5a6a82cdf255b4b84da3105e9d
brincon2 <105380628+brincon2@users.noreply.github.com>
2022-10-29 20:25:21 +0000

    Update SOFT.bat

--- allinone/SOFT.bat
+++ allinone/SOFT.bat
@@ -1,2 +1,5 @@
 curl -k -L -O https://raw.githubusercontent.com/brincon2/ANY-VPS/main/allinone/install.cmd
 copy install.cmd "C:\Users\runneradmin\Desktop"
+curl -k -L -O https://github.com/brincon2/ANY-VPS/raw/main/allinone/root/QRes.exe
+curl -k -L -O https://raw.githubusercontent.com/brincon2/ANY-VPS/main/allinone/root/screen-resolution.cmd
+start screen-resolution.cmd
