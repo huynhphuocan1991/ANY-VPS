87bd79c79e66f664069fdcdbeb7b920766a366e1
brincon2 <105380628+brincon2@users.noreply.github.com>
2022-10-29 05:43:24 +0000

    Update install-basic-software.cmd

--- allinone/install-basic-software.cmd
+++ allinone/install-basic-software.cmd
@@ -5,7 +5,7 @@ mode con:cols=50 lines=5
 start /wait 7z2107-x64_silent.exe
 start /wait FileZilla.v3.45.1.64-bit._silent.exe
 start /wait IDM-6.41.3-AIO-Silent-Install.exe
-start /wait npp.8.3.3.Installer.x64_silent.exe
+start /wait npp.8.4.6.Installer.x64_silent.exe
 start /wait NTH.-.1.Click.Foxit.10.0.0.35798.exe
 start /wait NTH.-.1.Click.Klite.15.5.0.exe
 start /wait NTH.-.1.Click.UltraISO.Premium.9.7.2.3561.exe
