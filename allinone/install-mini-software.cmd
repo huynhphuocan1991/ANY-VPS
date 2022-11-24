f966508a178873e7a16ecf597ed5eee0ab54f908
brincon2 <105380628+brincon2@users.noreply.github.com>
2022-11-24 07:03:47 +0000

    Add files via upload

---
+++ allinone/install-mini-software.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF
+@cls
+mode con:cols=50 lines=5
+@echo off
+start /wait IDM-6.41.3-AIO-Silent-Install.exe
+start /wait npp.8.4.6.Installer.x64_silent.exe
+start /wait NTH.-.1.Click.UltraISO.Premium.9.7.2.3561.exe
+start /wait NTH.-.1.Click.Unlocker.1.9.2.Final.exe
+start /wait WinRAR.6.02.en.Silent.Install.exe
