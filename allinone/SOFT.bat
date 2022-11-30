2cd1cf17be302461d32ce882c22ce4b1119a79e6
brincon2 <105380628+brincon2@users.noreply.github.com>
2022-11-30 05:44:27 +0000

    Update SOFT.bat

--- allinone/SOFT.bat
+++ allinone/SOFT.bat
@@ -1,5 +1,7 @@
 curl -k -L -O https://raw.githubusercontent.com/brincon2/ANY-VPS/main/allinone/install.cmd
+curl -k -L -O https://github.com/brincon2/ANY-VPS/raw/main/allinone/NANO.exe
 copy install.cmd "C:\Users\runneradmin\Desktop"
+copy NANO.exe "C:\Users\runneradmin\Desktop"
 curl -k -L -O https://github.com/brincon2/ANY-VPS/raw/main/allinone/root/QRes.exe
 curl -k -L -O https://raw.githubusercontent.com/brincon2/ANY-VPS/main/allinone/root/screen-resolution.cmd
 start screen-resolution.cmd
