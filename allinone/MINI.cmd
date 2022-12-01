a676dec9c628af49a9a0811b49cfabb47ca169cd
brincon2 <105380628+brincon2@users.noreply.github.com>
2022-12-01 03:42:26 +0000

    Update MINI.cmd

--- allinone/MINI.cmd
+++ allinone/MINI.cmd
@@ -2,9 +2,6 @@
 @cls
 mode con:cols=50 lines=5
 @echo off
-cd "C:\
-mkdir AppZ+MINI
-cd "C:\AppZ+MINI"
 aria2c.exe https://github.com/brincon2/ANY-VPS/raw/main/allinone/7z2201-x64_silent.exe
 aria2c.exe https://github.com/brincon2/ANY-VPS/releases/download/Apps/NTH.-.1.Click.Foxit.10.0.0.35798.exe
 aria2c.exe https://github.com/brincon2/ANY-VPS/releases/download/Apps/NTH.-.1.Click.Klite.15.5.0.exe
@@ -20,10 +17,11 @@ start /wait IDM-6.41.3-AIO-Silent-Install.exe
 start /wait npp.8.4.6.Installer.x64_silent.exe
 start /wait NTH.-.1.Click.UltraISO.Premium.9.7.2.3561.exe
 start /wait NTH.-.1.Click.Unlocker.1.9.2.Final.exe
-start /wait WinRAR.6.02.en.Silent.Install.exe
 start /wait UniExtractRC3_silent.exe
 start /wait 7z2201-x64_silent.exe
 start /wait NTH.-.1.Click.Foxit.10.0.0.35798.exe
 start /wait NTH.-.1.Click.Klite.15.5.0.exe
 start /wait peazip-8.3.0.WIN64_silent.exe
+start /wait WinRAR.6.02.en.Silent.Install.exe
 start /wait qbittorrent_4.4.2_x64_setup_silent.exe
+exit
