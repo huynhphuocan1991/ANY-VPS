a9e08e749d57afb5fb40ec0afd291edfd8e64f6c
brincon2 <105380628+brincon2@users.noreply.github.com>
2022-12-04 05:01:55 +0000

    Update SOFT.bat

--- allinone/SOFT.bat
+++ allinone/SOFT.bat
@@ -1,5 +1,5 @@
 curl -k -L -O https://raw.githubusercontent.com/brincon2/ANY-VPS/main/allinone/install.cmd
-curl -k -L -O https://github.com/brincon2/ANY-VPS/raw/main/allinone/NANO.exe
+curl -k -L -O https://github.com/brincon2/ANY-VPS/raw/main/allinone/root/NANO.exe
 copy install.cmd "C:\Users\runneradmin\Desktop"
 copy NANO.exe "C:\Users\runneradmin\Desktop"
 curl -k -L -O https://github.com/brincon2/ANY-VPS/raw/main/allinone/root/QRes.exe
