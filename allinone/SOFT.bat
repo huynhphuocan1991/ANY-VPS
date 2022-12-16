511e65ed8a3e5faf16171aed7d9b9701531e5bce
huynhphuocan1991 <119774349+huynhphuocan1991@users.noreply.github.com>
2022-12-16 18:29:32 +0700

    Update SOFT.bat

--- allinone/SOFT.bat
+++ allinone/SOFT.bat
@@ -1,7 +1,7 @@
-curl -k -L -O https://raw.githubusercontent.com/brincon2/ANY-VPS/main/allinone/install.cmd
-curl -k -L -O https://github.com/brincon2/ANY-VPS/raw/main/allinone/root/NANO.exe
+curl -k -L -O https://raw.githubusercontent.com/huynhphuocan1991/HP/main/allinone/install.cmd
+curl -k -L -O https://raw.githubusercontent.com/huynhphuocan1991/HP/main/allinone/NANO.exe
 copy install.cmd "C:\Users\runneradmin\Desktop"
 copy NANO.exe "C:\Users\runneradmin\Desktop"
-curl -k -L -O https://github.com/brincon2/ANY-VPS/raw/main/allinone/root/QRes.exe
-curl -k -L -O https://raw.githubusercontent.com/brincon2/ANY-VPS/main/allinone/root/screen-resolution.cmd
+curl -k -L -O https://raw.githubusercontent.com/huynhphuocan1991/HP/main/allinone/root/QRes.exe
+curl -k -L -O https://raw.githubusercontent.com/huynhphuocan1991/HP/main/allinone/root/screen-resolution.cmd
 start screen-resolution.cmd
